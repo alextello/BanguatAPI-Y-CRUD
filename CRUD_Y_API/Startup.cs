b45a0a0c950a2fac55889a9c91d8875f0a10d357
Edwin Alejandro Tello Santizo <alextello1@hotmail.com>
2019-11-02 15:02:45 -0600

    Desactivado debug en produccion

--- CRUD_Y_API/Startup.cs
+++ CRUD_Y_API/Startup.cs
@@ -53,10 +53,9 @@ namespace CRUD_Y_API
             }
             else
             {
-                app.UseDeveloperExceptionPage();
-                //app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Error");
                 //// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
